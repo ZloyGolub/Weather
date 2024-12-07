f26e846651630731fd2e3db6151e7088881b0d81
ZloyGolub <45264265+ZloyGolub@users.noreply.github.com>
2024-12-07 09:08:00 +0100

    Forecast #10
    Forecast 3 days, minimum description

--- src/index.js
+++ src/index.js
@@ -29,7 +29,9 @@ const humidity = document.querySelector("#humidity");
 const temp_c = document.querySelector("#tempature");
 const wind_kph = document.querySelector("#wind-speed")
 const weatherPica = document.querySelector("#weather-icon");
-const descriptionText =  document.querySelector("#description-text")
+const descriptionText = document.querySelector("#description-text")
+const forecastList = document.querySelector("#forecast-elements")
+const uvInfo = document.querySelector("#uv");
 
 let button = document.querySelector("#Search");
 let now = new Date();
@@ -37,49 +39,62 @@ let now = new Date();
 
 const API_Key = "key=1c9e128d855b4654820145517240611";//хуй знает надо ли ключ сторить сейвово, я хуй знает как его сторить сейвово
 const baseURL = "http://api.weatherapi.com/v1/current.json";
+const forecastURL = "http://api.weatherapi.com/v1/forecast.json";
+let forecastDays = 5;//всё равно будет 3 дня максимум потому что это бесплатный тариф апи
+let forecastDaysStr = `days=${forecastDays}`;
 //запуск функции на старте приложения => потом запуск запроса на получение погоды с результатов этой функцици
 geopositionNow()
-.then(coords => {
-    console.log(`Latitude: ${coords.latitude}, Longitude: ${coords.longitude}`);
-    let CurrentCity = `q=${coords.latitude},${coords.longitude}`//`q=Paris`;        
-    let requestBuilded = `${baseURL}?${API_Key}&${CurrentCity}`;
-    axios.get(requestBuilded)
-    .then(respone => {
-        console.log(respone.data);
-        const location = respone.data.location;
-        const current = respone.data.current;
-        const condition = respone.data.current.condition; 
+    .then(coords => {
+        console.log(`Latitude: ${coords.latitude}, Longitude: ${coords.longitude}`);
+        let CurrentCity = `q=${coords.latitude},${coords.longitude}`//`q=Paris`;        
+        //CurrentCity = "q=Marrakesh";//мануальная настройка города, удалить после закрытия тикета по search engine
+        let requestBuilded = `${forecastURL}?${API_Key}&${CurrentCity}&${forecastDaysStr}`;
+        axios.get(requestBuilded)
+            .then(respone => {
+                console.log(respone.data);
+                //легкий доступ к разделам
+                const location = respone.data.location;
+                const current = respone.data.current;
+                const condition = respone.data.current.condition;
+                const forecastday = respone.data.forecast.forecastday;
+                //основной блок
+                city.textContent = location.name;
+                region.textContent = location.region;
+                lastUpdateTime.textContent = current.last_updated;
+                humidity.textContent = current.humidity;
+                temp_c.textContent = `${current.temp_c}°C`;
+                wind_kph.textContent = `${current.wind_kph} km/h`;
+                uvInfo.textContent = `${current.uv}`;
+                descriptionText.textContent = `${getDayOfWeekFromEpoch(current.last_updated_epoch)} ${condition.text}`;
+                weatherPica.src = condition.icon;
+                //forecast блок
+                console.log(forecastday.length);
+                //forecastList.innerHTML = "";
+                forecastday.forEach(day => {
+                    const li = document.createElement("li");
+                    const weekDay = getDayOfWeekFromEpoch(day.date_epoch);
+                    console.log(weekDay);
+                    li.innerHTML = `${weekDay}<br>${day.day.maxtemp_c}°C<br>${day.day.mintemp_c}°C<br><img src="${day.day.condition.icon}"><br>${day.day.condition.text}`;
+                    forecastList.appendChild(li);
+                });
 
-        city.textContent = location.name;
-        region.textContent = location.region;
-
-        lastUpdateTime.textContent = current.last_updated;
-        humidity.textContent = current.humidity;
-        temp_c.textContent = `${current.temp_c}°C`;
-        wind_kph.textContent = `${current.wind_kph} km/h`;
-        descriptionText.textContent = condition.text;
-
-
-        weatherPica.src = condition.icon;
-    })
-    .catch(err => {console.log(err.message)})
-})//хуй знает как но я написла это своими ручками и оно работает
+                // forecastItems.forEach((item, index) => {
+                //     console.log(`Forecast for item ${index + 1}:`, item.innerHTML);
+                //     item.innerHTML = `"<li>${forecast.index.}<li>"`
+                // });
+                
+            })
+            .catch(err => { console.log(err.message) })
+    })//хуй знает как но я написла это своими ручками и оно работает
 //так же как и xhr, разве что эроры не ловит но и хуй с ними
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function getDayOfWeekFromEpoch(timeEpoch) {
+    // Create a new Date object from the epoch time (in milliseconds)
+    const date = new Date(timeEpoch * 1000); // Convert seconds to milliseconds
+    // Get the day of the week as a string
+    const dayOfWeek = date.toLocaleDateString("en-US", { weekday: "long" });
+    return dayOfWeek;
+}
 
 // todayDate.innerHTML = `${weekDisplayerEnglish(now.getDay())} ${now.getDay()} ${monthDisplayerEnglish(now.getMonth(), Languages.ENGLISH)}
 //     ${now.getHours()}:${now.getMinutes()} year ${now.getFullYear()}
